37ea2e18e419928b1eb2acc7c12200b88b2c3f5b
BleonaGerbavci <bg52732@ubt-uni.net>
2023-01-26 00:32:15 -0800

    Added Student model

---
+++ SoftwareDesignProject/SoftwareDesignProject/Data/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SoftwareDesignProject.Data.Models
+{
+    public class Student
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Key]
+        public int NrLeternjoftimit { get; set; }
+        public string Emri { get; set; }
+        public string EmriIPrindit { get; set; }
+        public string Mbiemri { get; set; }
+        public string Qyteti { get; set; }
+        public float NotaMesatare { get; set; }
+        public int NumriKontaktues { get; set; }
+        public string Email { get; set; }
+        public char Gjinia { get; set; }
+
+        // public bool isSpecalCategory { get; set; }
+        public int VitiIStudimeve { get; set; }
+        public string Statusi { get; set; }
+        public string ProfilePicUrl { get; set; }
+
+        //Fakulteti 
+
+    }
+}
---
+++ SoftwareDesignProject/SoftwareDesignProject/Migrations/20230126063007_InitialDbMigration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SoftwareDesignProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDbMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    NrLeternjoftimit = table.Column<int>(type: "int", nullable: false),
+                    Emri = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmriIPrindit = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Mbiemri = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Qyteti = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NotaMesatare = table.Column<float>(type: "real", nullable: false),
+                    NumriKontaktues = table.Column<int>(type: "int", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Gjinia = table.Column<string>(type: "nvarchar(1)", nullable: false),
+                    VitiIStudimeve = table.Column<int>(type: "int", nullable: false),
+                    Statusi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProfilePicUrl = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.NrLeternjoftimit);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
