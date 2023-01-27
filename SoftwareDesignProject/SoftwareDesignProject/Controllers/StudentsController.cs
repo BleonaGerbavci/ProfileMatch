3c25e78909b42297e977fd6d93c6927f6c1ac5fc
BleonaGerbavci <bg52732@ubt-uni.net>
2023-01-26 20:05:36 -0800

    added dependency injection

--- SoftwareDesignProject/SoftwareDesignProject/Controllers/StudentsController.cs
+++ SoftwareDesignProject/SoftwareDesignProject/Controllers/StudentsController.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿
 using Microsoft.AspNetCore.Mvc;
+using SoftwareDesignProject.Data;
 using SoftwareDesignProject.Data.Models;
 using SoftwareDesignProject.Data.Services;
 
@@ -9,47 +10,51 @@ namespace SoftwareDesignProject.Controllers
     [ApiController]
     public class StudentsController : ControllerBase
     {
-        public StudentsService _studentsServices;
+        //dependency injection 
+        private readonly AppDbContext _context;
+        private readonly IStudentService _studentService;
 
-        public StudentsController(StudentsService studentsServices)
+
+        public StudentsController(AppDbContext context, IStudentService studentService)
         {
-            _studentsServices = studentsServices;
+            _context = context;
+            _studentService = studentService;
         }
 
         [HttpGet("get-all-students")]
         public IActionResult GetAllStudents()
         {
-            var allStudents = _studentsServices.GetAllStudents();
+            var allStudents = _studentService.GetAllStudents();
             return Ok(allStudents);
         }
 
         [HttpGet("get-student-by-id/{nrLeternjoftimit}")]
         public IActionResult GetStudentById(int nrLeternjoftimit)
         {
-            var student = _studentsServices.GetStudentById(nrLeternjoftimit);
+            var student = _studentService.GetStudentById(nrLeternjoftimit);
             return Ok(student);
         }
 
         [HttpPost("add-student")]
         public IActionResult AddStudent([FromBody] Student student)
         {
-            _studentsServices.AddStudent(student);
+            _studentService.AddStudent(student);
             return Ok();
         }
 
         [HttpPut("update-student/{nrLeternjoftimit}")]
-        public IActionResult UpdateStudentById(int nrLeternjoftimit, [FromBody]Student student)
+        public IActionResult UpdateStudentById(int nrLeternjoftimit, [FromBody] Student student)
         {
-            var updatedStudent = _studentsServices.UpdateStudentById(nrLeternjoftimit, student);
+            var updatedStudent = _studentService.UpdateStudentById(nrLeternjoftimit, student);
             return Ok(updatedStudent);
         }
-      
+
 
 
         [HttpDelete("delete-student-by-id/{nrLeternjoftimit}")]
         public IActionResult DeleteStudentById(int nrLeternjoftimit)
         {
-            _studentsServices.DeleteStudentById(nrLeternjoftimit);
+            _studentService.DeleteStudentById(nrLeternjoftimit);
             return Ok();
         }
 
