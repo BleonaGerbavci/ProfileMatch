2711b24ac72b209dd316f551efe77a158265c16f
Alma <almanovoberdaliu@gmail.com>
2023-02-03 23:40:34 +0100

    added a method for taking the last 10 applicants

---
+++ SoftwareDesignProject/SoftwareDesignProject/Migrations/20230204034149_AddedInheritance.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SoftwareDesignProject.Migrations
+{
+    public partial class AddedInheritance : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "ListaEAplikanteve",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "ListaEAplikanteve");
+        }
+    }
+}
