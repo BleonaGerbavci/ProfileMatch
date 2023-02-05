920f4aa0f2f569dce887c6ee0ba54d5fd09a7101
MalisaSadiku <94755339+MalisaSadiku@users.noreply.github.com>
2023-02-05 22:24:21 +0100

    Added Konkursi in frontend

---
+++ SoftwareDesignProject/frontend/src/features/Konkursi/Konkursi.jsx
@@ -0,0 +1,101 @@
+import react from "react";
+import './Konkursi.css';
+
+
+export default function Konkursi() {
+  return (
+
+    <div>
+    <h4 className= "Qendra" >Qendra e Studenteve ne Prishtine shpalle:</h4>
+     <h1 className="Konkursi">KONKURS</h1>
+     <p className="paragrafi1">Per pranimin e studenteve ne konvikte dhe ushqim ne mensen e studenteve per vitin <br />
+    Akademik 2022/2023.
+     Aplikimi behet permes sistemit ONLINE. Linku per aplikim eshte i publikuar ne web <br />
+     faqen zyrtare te QS-se qs.rks-gov.net dhe ne profilin zyrtar ne facebook Qendra e <br />
+     Studenteve-Prishtine. Udhezuesi per aplikim gjendet ne linkun e shfaqur. <br />
+     Kete vit Akademik, studentet e rregullt te UP-se, kane mundesi te aplikojne veçmas per <br />
+     te fituar te drejten per tu ushqyer ne mensen e studenteve. <br />
+     Kriteret per te aplikuar jane te njejta sikurse per aplikim per konvikt. <br />
+     Numri i studenteve qe do te perfitojne per ushqim ne mense, eshte 1500 student. <br />
+     Ne konviktet dhe mensen e QS-se pranohen studentet e rregullt te studimeve themelore, <br />
+     komuna e te cileve eshte 25 km e me larg nga Prishtina.</p>
+
+
+     <h3 className="kriteret"> Kriteret per pranim dhe piket e mundshme:</h3>
+     <p className="paragrafi2">
+        1. Studentet nga Fakulteti i Mjekesise, Fakulteti Teknik dhe Fakulteti i Shkencave <br />
+Matematike-Natyrore me note mesatare 9.00 dhe me largesi 25 km e me shumë nga <br />
+Prishtina, pranohen ne konvikte pavaresisht totalit të pikeve qe fitojne. <br />
+ 2. Studentet nga fakultetet e tjera me note mesatare 9.5 dhe me largesi 25 km e me shume nga Prishtina pranohen ne konvikte pavaresisht numrit te pikeve te fituara. <br />
+ 3. Studentet per note mesatare fitojne (varesisht nga nota mesatare)…5 deri 15 pike <br />
+ 4. Largesia nga Komuna e Prishtines……… 2 deri 10 pike 
+     </p>
+
+     <h3 className="statusi">Studentet me status te vaçant per pike shtese sipas kategorive:</h3>
+
+     <p className="paragrafi3">
+     1. student (femije) i deshmorit ------------------------------------------10 pike <br />
+     2. student (femije) i prindit invalid te luftes ---------------------------5 pike <br />
+     3. student, prindi i te cilit eshte veteran i luftes ------------------------3 pike <br />
+     4. student i te pagjeturit ---------------------------------------------------5 pike <br />
+     5. student invalid civil i luftes -------------------------------------------5 pike <br />
+     6. student i prindit martir nga lufta --------------------------------------4 pike <br />
+     7. student me aftesi te kufizuara -----------------------------------------6 pike <br />
+     8. student i te burgosurit, politik -----------------------------------4 pike <br />
+     9. student me asistence sociale ------------------------------------------4 pike <br />
+     10.dy e me shume studente nga nje familje aplikant ne QS ……….3 pike 
+     </p>
+    
+     <p className="paragrafi4">Te gjithe studentet e ketyre kategorive te cilet perfitojn pike shtese duhet qe permes <br />
+     sistemit ONLINE te ngarkojne dokumentacionin e kerkuar (vertetimin)nga :
+     </p>
+
+     <p className="paragrafi5">• Shoqata e te Burgosurve Politik, zyra qendrore Prishtine <br />
+• Shoqata e personave me Aftesi te Kufizuara – Hendikos, zyra qendrore Prishtine. <br />
+• Shoqata e familjeve te deshmoreve – Selia qendrore ne Prishtine <br />
+.Studenti i prindit martir nga lufta, student i te pagjeturit, student invalid civil i luftes <br />
+vendimi i MPMS dhe çertifikata e bashkesise familjare.
+• Studentet , prinderit e te cileve jane veteran te Luftes , e te cilet marrin page nga <br />
+Buxheti i Republikes se Kosoves, e ngarkojne çertifikaten e veteranit te prindit , te <br />
+leshuar nga Qeveria e Republikes se Kosoves, ndersa studentet prinderit e te cileve nuk <br />
+marrin page nga Buxheti i Republikes se Kosoves nuk kane nevoj t’a bejne ngarkimin e <br />
+çertifikates se prindit veteran, sepse piket i’u llogariten nga vet sistemi, nga te dhenat e
+MPMS.</p>
+
+<p className="paragrafi6"> Sqarim: Njoftojme studentet se piket e fituara nga nota mesatare, nga largesia, student i <br />
+prindit invalid te luftes, studenteve me asistence sociale piket i’u llogariten permes sistemit <br />
+online. Andaj nuk eshte e nevojshme qe keto dokumente te ngarkohen gjate aplikimit.</p>
+
+<p className="paragrafi7">Te kategoria dy e me shume studente nga nje familje, aplikuesi per vendosje ne QS <br />
+me rastin e aplikimit studenti i dyte apo i trete nga e njejta familje, duhet te shkruaj nr. <br />
+e leternjoftimit te aplikantit te pare , ne opsionin e hapur per kete kategori.</p>
+
+<p className="paragrafi8">
+• Konkursi mbetet i hapur deri me daten 18.09.2023 nga dita e shpalljes.
+. Studentet qe kane thyer Rregulloret ne fuqi ne Qendren e Studenteve nuk do te
+pranohen ne konvikte.
+• Ne konvikte do te pranohen studentet me numrin me te madh te pikeve te fituara.
+Publikimi i rezultateve behet me date 26.09.2023 ne tabelat e konvikteve dhe faqet
+zyrtare te QSP.
+• Afati per ushtrimin e ankesave ONLINE eshte 27 – 28.09. 2023.
+• Publikimi i rezultateve te ankesave behet me daten 03.10.2023.
+</p>
+
+<h5 className="drejtoria">Drejtoria e Qendres se Studenteve, Prishtine</h5>
+</div>
+
+
+
+
+
+
+  )
+
+
+
+
+
+
+
+
+}
